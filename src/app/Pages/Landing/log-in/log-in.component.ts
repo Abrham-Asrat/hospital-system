bc7f0bdf18ff42bf9568d8b8fe8dc3f74c7d21d1
Eden42de <edendechasa571@gmail.com>
2025-03-06 17:02:11 -0800

    login added

--- src/app/Pages/Landing/log-in/log-in.component.ts
+++ src/app/Pages/Landing/log-in/log-in.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-})
+
 export class LoginComponent {
   loginForm: FormGroup;
   isSubmitted = false;
