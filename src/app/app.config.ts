272eea38d02f831548b28b52396805690d9d2b81
Biniam Tomas <bini49872@gmail.com>
2025-04-28 14:12:19 +0300

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideZoneChangeDetection } from '@angular/core';
 import { importProvidersFrom } from '@angular/core';
-import { routes } from './app.routes';
+import { routes } from './routes';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 
---
+++ src/app/routes.ts
@@ -0,0 +1,107 @@
+import { Routes } from '@angular/router';
+import { DAppointmentComponent } from './Pages/DoctorRole/d-appointment/d-appointment.component';
+import { DBlogComponent } from './Pages/DoctorRole/d-blog/d-blog.component';
+import { DChatComponent } from './Pages/DoctorRole/d-chat/d-chat.component';
+import { DContactComponent } from './Pages/DoctorRole/d-contact/d-contact.component';
+import { DHomeComponent } from './Pages/DoctorRole/d-home/d-home.component';
+import { DLayoutComponent } from './Pages/DoctorRole/d-layout/d-layout.component';
+import { DNotificationComponent } from './Pages/DoctorRole/d-notification/d-notification.component';
+import { DProfileComponent } from './Pages/DoctorRole/d-profile/d-profile.component';
+import { LAboutComponent } from './Pages/Landing/about/l-about/l-about.component';
+import { LBlogComponent } from './Pages/Landing/l-blog/l-blog.component';
+import { LContactComponent } from './Pages/Landing/l-contact/l-contact.component';
+import { FaqComponent } from './Pages/Landing/l-faq/l-faq.component';
+import { LHomeComponent } from './Pages/Landing/l-home/l-home.component';
+import { LLayoutComponent } from './Pages/Landing/l-layout/l-layout.component';
+import { LoginComponent } from './Pages/Landing/log-in/log-in.component';
+import { SignUpComponent } from './Pages/Landing/sign-up/sign-up.component';
+import { PAppointmentComponent } from './Pages/Patient/p-appointment/p-appointment.component';
+import { PBlogComponent } from './Pages/Patient/p-blog/p-blog.component';
+import { PChatComponent } from './Pages/Patient/p-chat/p-chat.component';
+import { PContactUsComponent } from './Pages/Patient/p-contact-us/p-contact-us.component';
+import { PHomeComponent } from './Pages/Patient/p-home/p-home.component';
+import { PLayoutComponent } from './Pages/Patient/p-layout/p-layout.component';
+import { PNotificationComponent } from './Pages/Patient/p-notification/p-notification.component';
+import { PProfileComponent } from './Pages/Patient/p-profile/p-profile.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'Dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'Dashboard',
+    component: LLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'Home',
+      },
+      {
+        path: 'Home',
+        component: LHomeComponent,
+      },
+      {
+        path: 'Blog',
+        component: LBlogComponent,
+      },
+      {
+        path: 'About',
+        component: LAboutComponent,
+      },
+      {
+        path: 'Contact',
+        component: LContactComponent,
+      },
+      {
+        path: 'FAQ',
+        component: FaqComponent,
+      },
+      {
+        path: 'logIn',
+        component: LoginComponent,
+      },
+      {
+        path: 'SignUp',
+        component: SignUpComponent,
+      },
+    ],
+  },
+
+  {
+    path: 'Patient',
+    component: PLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'Home',
+      },
+      { path: 'Home', component: PHomeComponent },
+      { path: 'Appointment', component: PAppointmentComponent },
+      { path: 'Blog', component: PBlogComponent },
+      { path: 'Chat', component: PChatComponent },
+      { path: 'Contact', component: PContactUsComponent },
+      { path: 'Notification', component: PNotificationComponent },
+      { path: 'Profile', component: PProfileComponent },
+    ],
+  },
+  {
+    path: 'Doctor',
+    component: DLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'Home',
+      },
+      { path: 'Home', component: DHomeComponent },
+      { path: 'Appointment', component: DAppointmentComponent },
+      { path: 'Blog', component: DBlogComponent },
+      { path: 'Chat', component: DChatComponent },
+      { path: 'Contact', component: DContactComponent },
+      { path: 'Notification', component: DNotificationComponent },
+      { path: 'Profile', component: DProfileComponent },
+    ],
+  },
+];
