94fd9f16380395e86972ea6ea70d62de89c628af
Naftannafi <149290646+Naftannafi@users.noreply.github.com>
2025-06-24 15:36:08 +0300

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { enableProdMode } from '@angular/core';
-import { AppComponent } from './app/app.component';
-
-enableProdMode();
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
